4465cfdd48c79f4fdfe158c232df8be3c5af6284
naif19ta <juan.patino.dc@gmail.com>
2025-07-06 00:42:39 -0500

    on decolle

---
+++ cxa.h
@@ -0,0 +1,9 @@
+/*
+ * cxa - C executable arguments parser
+ * Jul 6, 2025
+ * Whole file jaja
+ */
+#ifndef CXA_H
+#define CXA_H
+
+#endif
---
+++ tester.c
@@ -0,0 +1,6 @@
+#include "cxa.h"
+
+int main (int narg, char **args)
+{
+	return 0;
+}
