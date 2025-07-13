55835f2dab30faa337c35f8fc63f2ad3489c99b1
naif19ta <juan.patino.dc@gmail.com>
2025-07-13 12:56:00 -0500

    bitfields families created

--- source/cxa.h
+++ source/cxa.h
@@ -23,28 +23,37 @@ extern "C" {
 /* Specifies if the flag takes an argument
  * ex: --source=arg or -s arg
  */
-#define CXA_FLAG_TAKER_NON      0
-#define CXA_FLAG_TAKER_MAY      1
-#define CXA_FLAG_TAKER_YES      2
+#define CXA_FLAG_TAKER_NON      0b00000000
+#define CXA_FLAG_TAKER_MAY      0b00000001
+#define CXA_FLAG_TAKER_YES      0b00000010
 
 /* Specifies the type expected of the argument
  * (only makes sense if CxaFlagHasArg is set to 1)
  */
-#define CXA_FLAG_ARG_TYPE_STR   0
-#define CXA_FLAG_ARG_TYPE_CHR   1
-#define CXA_FLAG_ARG_TYPE_SHT   2
-#define CXA_FLAG_ARG_TYPE_INT   3
-#define CXA_FLAG_ARG_TYPE_LNG   4
-#define CXA_FLAG_ARG_TYPE_DBL   5
+#define CXA_FLAG_ARG_TYPE_STR   0b00000000
+#define CXA_FLAG_ARG_TYPE_CHR   0b00000100
+#define CXA_FLAG_ARG_TYPE_SHT   0b00001000
+#define CXA_FLAG_ARG_TYPE_INT   0b00001100
+#define CXA_FLAG_ARG_TYPE_LNG   0b00010000
+#define CXA_FLAG_ARG_TYPE_DBL   0b00010100
 
 /* When the library already has parsed all flags the programmer
  * will need to know whether the flag was given or not
  */
-#define CXA_FLAG_WASNT_SEEN     0
-#define CXA_FLAG_WAS_SEEN       1
+#define CXA_FLAG_WASNT_SEEN     0b00000000
+#define CXA_FLAG_WAS_SEEN       0b00100000
 
-#define CXA_FLAG_ARG_GIVEN_NON  0
-#define CXA_FLAG_ARG_GIVEN_YES  1
+#define CXA_FLAG_ARG_GIVEN_NON  0b00000000
+#define CXA_FLAG_ARG_GIVEN_YES  0b01000000
+
+/* The programmer which is using this library should only
+ * use CXA_FLAG_SEEN_MASK in their caller function, others
+ * are only meaningful here
+ */
+#define CXA_FLAG_TAKER_MASK     0b00000011
+#define CXA_ARG_TYPE_MASK       0b00011100
+#define CXA_FLAG_SEEN_MASK      0b00100000
+#define CXA_FLAG_ARG_GIVEN_MASK 0b01000000
 
 /* Factor in which the positional arguments grows
  */
@@ -56,15 +65,14 @@ typedef unsigned char CxaFlagArgType;
 typedef unsigned char CxaFlagWasSeen;
 typedef unsigned char CxaFlagArgGiven;
 
+typedef unsigned char CxaFlagMeta;
+
 struct CxaFlag
 {
 	char            *longname;
 	char            *description;
 	void            *destination;
-	CxaFlagWasSeen  seen;
-	CxaFlagHasArg   has;
-	CxaFlagArgType  argtype;
-	CxaFlagArgGiven argiven;
+	CxaFlagMeta     meta;
 	char            shortname;
 };
 
